0846a97c93df891822d9c45ba621df9213a3bc4e
Christian Gregg <c_arlt@hotmail.com>
2018-08-25 10:42:41 +0100

    Setup fish shell

---
+++ config/fish/conf.d/abbr.fish
@@ -0,0 +1,11 @@
+if status --is-interactive
+  set -g fish_user_abbreviations
+
+  abbr g git
+  abbr b bundle
+  abbr be 'bundle exec'
+  abbr bi 'bundle install'
+  abbr v vim
+  abbr clr clear
+  abbr t tmux
+end
