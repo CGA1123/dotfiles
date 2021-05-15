d49c4bb464ad5321f8746fdf489db49db889971e
Christian Gregg <christian@bissy.io>
2021-05-15 19:51:11 +0100

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,4 +3,18 @@ set PATH "$HOME/.dotfiles/bin" "$GOPATH/bin" $PATH
 set EDITOR "vim"
 set VISUAL "vim"
 
+set -gx HOMEBREW_PREFIX "/opt/homebrew";
+set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+if test -d (brew --prefix)"/share/fish/completions"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
