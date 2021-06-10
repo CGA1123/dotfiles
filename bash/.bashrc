359002d0bf4e3d0669bc57288a6c58512dce86a0
Christian Gregg <christian@bissy.io>
2021-06-10 14:34:29 +0100

    bash??

--- bash/.bashrc
+++ bash/.bashrc
@@ -1,5 +1,6 @@
 source "${HOME}/.colour.sh"
 source "${HOME}/.abbreviations.sh"
 source "${HOME}/.git-prompt.sh"
+source "${HOME}/.prompt.sh"
 
 PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
--- bash/.prompt.sh
+++ bash/.prompt.sh
@@ -6,3 +6,45 @@ SYMB_GIT_STASHED="╍╍"
 SYMB_GIT_AHEAD="+ "
 SYMB_GIT_BEHIND="- "
 SYMB_GIT_AHEAD_BEHIND="-+ "
+
+function git_branch_name() {
+  git symbolic-ref --short HEAD 2>/dev/null \
+    || command git describe --tags --exact-match HEAD 2>/dev/null \
+    || command git rev-parse --short HEAD 2>/dev/null
+}
+
+function git_is_repo() {
+  git rev-parse --git-dir > /dev/null 2>/dev/null
+}
+
+function git_is_staged() {
+  git_is_repo && ! git diff --cached --no-ext-diff --quiet --exit-code 2>/dev/null
+}
+
+function git_is_dirty() {
+  git_is_repo && ! git diff --no-ext-diff --quiet --exit-code 2>/dev/null
+
+}
+
+function git_is_touched() {
+  git_is_staged || git_is_dirty
+}
+
+function git_is_detached_head() {
+  ! git symbolic-ref --quiet HEAD >/dev/null 2>&1
+}
+
+function git_is_stashed() {
+  git rev-parse --verify --quiet refs/stash > /dev/null 2>/dev/null
+}
+
+# TODO:
+# f segment
+# f segment_close
+# f segment_right
+# f segment_right
+# gv SEGMENT
+# gv SEGMENT_COLOR
+#
+# - last status
+# - last command duration
