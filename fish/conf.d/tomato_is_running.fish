4c7a9697bb7546b1a6145dc8774d102a644b5a38
Christian Gregg <christian@bissy.io>
2021-06-11 23:26:46 +0100

    rm fish

--- fish/conf.d/tomato_is_running.fish
+++
@@ -1,7 +0,0 @@
-function tomato_is_running
-  if command tomato running ^ /dev/null > /dev/null
-    return 0
-  else
-    return 1
-  end
-end
--- fish/conf.d/tomato_is_up.fish
+++
@@ -1,7 +0,0 @@
-function tomato_is_up
-  if command tomato up ^ /dev/null > /dev/null
-    return 0
-  else
-    return 1
-  end
-end
